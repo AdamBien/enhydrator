e9cc81524996f87c645f8dfe316d1e3338dbed2d
Adam Bien <abien@adam-bien.com>
2014-04-19 14:51:09 +0200

    Tests for emptyExpression added

--- enhydrator/src/test/java/com/airhacks/enhydrator/transform/ExpressionTest.java
+++ enhydrator/src/test/java/com/airhacks/enhydrator/transform/ExpressionTest.java
@@ -2,10 +2,12 @@ package com.airhacks.enhydrator.transform;
 
 import com.airhacks.enhydrator.in.Entry;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 import org.junit.Before;
+import org.junit.Test;
 
 /**
  *
@@ -22,7 +24,7 @@ public class ExpressionTest {
 
     @Test
     public void bindingsAreWorking() {
-        String expression = "print(current); print(columns);";
+        String expression = "print(current); print(columns);java.util.Collections.EMPTY_LIST";
         Entry e1 = new Entry(0, "chief", 42, "duke");
         Entry e2 = new Entry(1, "master", 21, "juggy");
         List<Entry> input = new ArrayList<>();
@@ -36,8 +38,16 @@ public class ExpressionTest {
     public void emptyList() {
         Entry e1 = new Entry(0, "chief", 42, "duke");
         List<Entry> input = new ArrayList<>();
-        List<Entry> execute = this.cut.execute(input, e1, "");
+        List<Entry> execute = this.cut.execute(input, e1, "java.util.Collections.EMPTY_LIST");
         assertTrue(execute.isEmpty());
+    }
+
+    @Test
+    public void emptyExpression() {
+        Entry e1 = new Entry(0, "chief", 42, "duke");
+        List<Entry> input = new ArrayList<>();
+        List<Entry> result = this.cut.execute(input, e1, "");
+        assertThat(result, is(e1.asList()));
     }
 
 }
