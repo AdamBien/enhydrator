ac9b34c3492438343888e091d7a2a8acdea2ea9d
Adam Bien <abien@adam-bien.com>
2014-04-27 08:12:36 +0200

    toString implemented

--- enhydrator/src/main/java/com/airhacks/enhydrator/in/VirtualSinkSource.java
+++ enhydrator/src/main/java/com/airhacks/enhydrator/in/VirtualSinkSource.java
@@ -9,9 +9,9 @@ package com.airhacks.enhydrator.in;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -74,7 +74,12 @@ public class VirtualSinkSource extends Sink implements Source {
 
     @Override
     public String toString() {
-        return this.rows.stream().map(l -> mapToString(l)).reduce((l, r) -> l + "\n" + r).get();
+        Optional<String> representation = this.rows.stream().map(l -> mapToString(l)).reduce((l, r) -> l + "\n" + r);
+        if (!representation.isPresent()) {
+            return "--empty--";
+        } else {
+            return representation.get();
+        }
     }
 
     public String mapToString(List<Entry> columns) {
