82fafca82f05892242160f49e11091d7fb664144
Adam Bien <abien@adam-bien.com>
2014-05-02 06:08:31 +0200

    Row conversion fixed

--- enhydrator/src/main/java/com/airhacks/enhydrator/in/ResultSetToEntries.java
+++ enhydrator/src/main/java/com/airhacks/enhydrator/in/ResultSetToEntries.java
@@ -9,9 +9,9 @@ package com.airhacks.enhydrator.in;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -33,36 +33,33 @@ public class ResultSetToEntries implements Function<ResultSet, Row> {
 
     @Override
     public Row apply(ResultSet resultSet) {
-        Row row = null;
+        Row row = new Row(0);
         try {
             final ResultSetMetaData metaData = resultSet.getMetaData();
             int columnCount = metaData.getColumnCount();
-            int slot;
             for (int i = 1; i <= columnCount; i++) {
                 //from java.sql.Types
                 int columnType = metaData.getColumnType(i);
                 String columnName = metaData.getColumnName(i);
-                slot = i - 1;
-                row = new Row(i);
                 switch (columnType) {
                     case Types.VARCHAR:
                     case Types.CHAR:
-                        row.addColumn(columnName, resultSet.getString(columnType));
+                        row.addColumn(columnName, resultSet.getString(i));
                         break;
                     case Types.INTEGER:
-                        row.addColumn(columnName, resultSet.getInt(columnType));
+                        row.addColumn(columnName, resultSet.getInt(i));
                         break;
                     case Types.DOUBLE:
-                        row.addColumn(columnName, resultSet.getDouble(columnType));
+                        row.addColumn(columnName, resultSet.getDouble(i));
                         break;
                     case Types.BOOLEAN:
-                        row.addColumn(columnName, resultSet.getBoolean(columnType));
+                        row.addColumn(columnName, resultSet.getBoolean(i));
                         break;
                     case Types.FLOAT:
-                        row.addColumn(columnName, resultSet.getFloat(columnType));
+                        row.addColumn(columnName, resultSet.getFloat(i));
                         break;
                     default:
-                        row.addColumn(columnName, String.valueOf(resultSet.getObject(columnType)));
+                        row.addColumn(columnName, resultSet.getObject(i));
                 }
             }
         } catch (SQLException ex) {
