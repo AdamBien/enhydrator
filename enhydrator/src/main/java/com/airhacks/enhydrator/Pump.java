14cdaf783cfa5bdbdb93ac2efed85e90d1b9fecc
Adam Bien <abien@adam-bien.com>
2014-05-06 09:43:17 +0200

    Filter execution order fixed

--- enhydrator/src/main/java/com/airhacks/enhydrator/Pump.java
+++ enhydrator/src/main/java/com/airhacks/enhydrator/Pump.java
@@ -9,9 +9,9 @@ package com.airhacks.enhydrator;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -193,7 +193,7 @@ public class Pump {
         if (trafos == null || trafos.isEmpty()) {
             return convertedColumns;
         }
-        final Function<Row, Row> composition = trafos.stream().reduce((i, j) -> i.compose(j)).get();
+        final Function<Row, Row> composition = trafos.stream().reduce((i, j) -> i.andThen(j)).get();
         Row result = composition.apply(convertedColumns);
         if (result == null) {
             return null;
