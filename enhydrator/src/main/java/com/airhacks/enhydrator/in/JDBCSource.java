2f408966deb9497d77fde5906d60c23f9d480209
Adam Bien <abien@adam-bien.com>
2014-05-12 11:28:53 +0200

    Name of tag in xml changed

--- enhydrator/src/main/java/com/airhacks/enhydrator/in/JDBCSource.java
+++ enhydrator/src/main/java/com/airhacks/enhydrator/in/JDBCSource.java
@@ -9,9 +9,9 @@ package com.airhacks.enhydrator.in;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -34,7 +34,7 @@ import javax.xml.bind.annotation.XmlRootElement;
  * @author airhacks.com
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlRootElement
+@XmlRootElement(name = "jdbc-source")
 public class JDBCSource implements Source {
 
     @XmlAttribute
@@ -134,4 +134,10 @@ public class JDBCSource implements Source {
             return source;
         }
     }
+
+    @Override
+    public String toString() {
+        return "JDBCSource{" + "name=" + name + ", connectionProvider=" + connectionProvider + '}';
+    }
+
 }
