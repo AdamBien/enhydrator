892ccb3c1541f814eb60c021434a7291da2dc1de
Adam Bien <abien@adam-bien.com>
2014-05-23 20:59:24 +0200

    Null check for input row implemented

--- enhydrator/src/main/java/com/airhacks/enhydrator/transform/FunctionScriptLoader.java
+++ enhydrator/src/main/java/com/airhacks/enhydrator/transform/FunctionScriptLoader.java
@@ -9,9 +9,9 @@ package com.airhacks.enhydrator.transform;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -65,14 +65,15 @@ public class FunctionScriptLoader {
 
     public RowTransformer getRowTransformer(String scriptName) {
         return (Row input) -> {
+            if (input == null) {
+                return null;
+            }
             String content = load(ROW_SCRIPT_FOLDER, scriptName);
             try {
                 Bindings bindings = engine.createBindings();
                 bindings.put("$EMPTY", new Row());
-                if (input != null) {
-                    input.getColumns().forEach(c -> bindings.put(c.getName(), c));
-                    bindings.put("$ROW", input);
-                }
+                input.getColumns().forEach(c -> bindings.put(c.getName(), c));
+                bindings.put("$ROW", input);
                 return (Row) engine.eval(content, bindings);
             } catch (ScriptException ex) {
                 throw new IllegalStateException("Cannot evaluate script", ex);
