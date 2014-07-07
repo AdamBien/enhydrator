953b2bfbcebaf02006968eaf5e21da0f3e9c328c
Adam Bien <abien@adam-bien.com>
2014-07-07 22:58:12 +0200

    Row logging removed

--- enhydrator/src/main/java/com/airhacks/enhydrator/Pump.java
+++ enhydrator/src/main/java/com/airhacks/enhydrator/Pump.java
@@ -9,9 +9,9 @@ package com.airhacks.enhydrator;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -122,7 +122,6 @@ public class Pump {
 
     void onNewRow(Row row) {
         row.useMemory(memory);
-        this.flowListener.accept("Received: " + row);
         this.flowListener.accept("Processing: " + row.getNumberOfColumns() + " columns !");
         Optional<Boolean> first = this.filterExpressions.stream().
                 map(e -> this.filterExpression.execute(row, e)).
