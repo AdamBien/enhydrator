0ff416f3926b91d5a969b1143217b72f2b3cf26a
TsoumakisD <TsoumakisD@HELPC2680.hel.Diehl.Corp>
2014-08-11 09:55:32 +0200

    Fixed typo in datatype-mapper

--- enhydrator/src/main/java/com/airhacks/enhydrator/transform/DatatypeMapper.java
+++ enhydrator/src/main/java/com/airhacks/enhydrator/transform/DatatypeMapper.java
@@ -19,9 +19,9 @@ import javax.xml.bind.annotation.XmlRootElement;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -34,7 +34,7 @@ import javax.xml.bind.annotation.XmlRootElement;
  * @author airhacks.com
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlRootElement(name = "destination-mapper")
+@XmlRootElement(name = "datatype-mapper")
 public class DatatypeMapper extends RowTransformation {
 
     private Map<Integer, Datatype> mappings;
