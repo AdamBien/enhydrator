5281f8da49c7d5016972377c33cd7eb1a329d5da
Adam Bien <abien@adam-bien.com>
2014-09-30 11:03:00 -0700

    Json to log example implemented

---
+++ samples/JSON2Object/src/test/java/com/airhacks/samples/json/Language.java
@@ -0,0 +1,20 @@
+package com.airhacks.samples.json;
+
+/**
+ *
+ * @author airhacks.com
+ */
+public class Language {
+
+    private String java;
+    private int rank;
+
+    public int getRank() {
+        return rank;
+    }
+
+    public String getJava() {
+        return java;
+    }
+
+}
---
+++ samples/JSON2Object/src/test/resources/converter.js
@@ -0,0 +1,32 @@
+/*
+ * #%L
+ * enhydrator
+ * %%
+ * Copyright (C) 2014 Adam Bien
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+var Row = Java.type("com.airhacks.enhydrator.in.Row");
+print(INPUT);
+var row = new Row();
+var results = JSON.parse(INPUT);
+var counter = 0;
+for (result in results.methods) {
+    print(result + " - " + results.methods[result]);
+    row.addColumn(counter++, result, results.methods[result]);
+
+}
+ROWS.add(row);
+ROWS;
+
