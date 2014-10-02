198c91ef5c773f89e2faade6a327abf6589ae38d
Adam Bien <abien@adam-bien.com>
2014-10-01 21:22:28 -0700

    Naming fixed

--- enhydrator/src/main/java/com/airhacks/enhydrator/Pump.java
+++ enhydrator/src/main/java/com/airhacks/enhydrator/Pump.java
@@ -9,9 +9,9 @@ package com.airhacks.enhydrator;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -290,8 +290,8 @@ public class Pump {
             return this;
         }
 
-        public Engine with(String entryName, Function<Object, Object> entryFunction) {
-            this.entryFunctions.put(entryName, entryFunction);
+        public Engine with(String columnName, Function<Object, Object> entryFunction) {
+            this.entryFunctions.put(columnName, entryFunction);
             return this;
         }
 
