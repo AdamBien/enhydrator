668eec4b60687c45fea381116cf92f5061cf6da4
ZeTo <tobias.zeck@diehl.com>
2015-05-15 18:20:56 +0200

    Added test for CsvFileSink charset parameter.

---
+++ samples/json2csv/src/test/java/com/airhacks/samples/json/CsvTest.java
@@ -0,0 +1,12 @@
+package com.airhacks.samples.json;
+
+/**
+ * @author zeto
+ */
+public class CsvTest {
+    final static String INPUT = "./src/test/resources/";
+
+    protected String getFileName() {
+        return "./target/" + System.currentTimeMillis() + "output.csv";
+    }
+}
