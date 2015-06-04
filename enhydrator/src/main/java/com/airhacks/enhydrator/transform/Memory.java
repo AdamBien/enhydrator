f406e50f33f68019ead466e57d574721bf5b54c9
Adam Bien <abien@adam-bien.com>
2015-06-04 19:02:48 +0200

    toString implemented

--- enhydrator/src/main/java/com/airhacks/enhydrator/transform/Memory.java
+++ enhydrator/src/main/java/com/airhacks/enhydrator/transform/Memory.java
@@ -9,9 +9,9 @@ package com.airhacks.enhydrator.transform;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -101,4 +101,8 @@ public class Memory {
         return this.processingErrors.keySet();
     }
 
+    @Override
+    public String toString() {
+        return "Memory{" + "store=" + store + ", processingErrors=" + processingErrors + ", counter=" + counter + ", processedRowCount=" + processedRowCount + ", errorCount=" + errorCount + '}';
+    }
 }
