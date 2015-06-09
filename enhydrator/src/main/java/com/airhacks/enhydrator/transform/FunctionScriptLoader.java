7fc3a3d194af8d047a3877bbed34489b0266929d
Adam Bien <abien@adam-bien.com>
2015-06-09 07:47:40 +0200

    NPE in case of null bindings fixed

--- enhydrator/src/main/java/com/airhacks/enhydrator/transform/FunctionScriptLoader.java
+++ enhydrator/src/main/java/com/airhacks/enhydrator/transform/FunctionScriptLoader.java
@@ -9,9 +9,9 @@ package com.airhacks.enhydrator.transform;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -70,7 +70,9 @@ public abstract class FunctionScriptLoader {
     public ColumnTransformer createFromScript(String script) {
         Invocable invocable = (Invocable) engine;
         try {
-            manager.getBindings().putAll(scriptEngineBindings);
+            if (scriptEngineBindings != null) {
+                manager.getBindings().putAll(scriptEngineBindings);
+            }
             engine.eval(script);
         } catch (ScriptException ex) {
             throw new IllegalStateException("Cannot evaluate script", ex);
@@ -82,7 +84,9 @@ public abstract class FunctionScriptLoader {
     public ColumnTransformer createFromScript(Reader script) {
         Invocable invocable = (Invocable) engine;
         try {
-            manager.getBindings().putAll(scriptEngineBindings);
+            if (scriptEngineBindings != null) {
+                manager.getBindings().putAll(scriptEngineBindings);
+            }
             engine.eval(script);
         } catch (ScriptException ex) {
             throw new IllegalStateException("Cannot evaluate script", ex);
--- enhydrator/src/test/java/com/airhacks/enhydrator/flexpipe/PipelineTest.java
+++ enhydrator/src/test/java/com/airhacks/enhydrator/flexpipe/PipelineTest.java
@@ -9,9 +9,9 @@ package com.airhacks.enhydrator.flexpipe;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -124,7 +124,7 @@ public class PipelineTest {
         origin.addEntryTransformation(e2);
         origin.addPreRowTransformation(targetMapper);
         origin.addPreRowTransformation(datatypeMapper);
-        origin.addEntryTransformation(new ColumnTransformation("1", "return 42", false));
+        origin.addEntryTransformation(new ColumnTransformation("1", "function execute(i){return 42;}", false));
         origin.addPreRowTransformation(new NashornRowTransformer("src/test/scripts", "encrypt"));
         origin.addPostRowTransformation(new NashornRowTransformer("src/test/scripts", "compress"));
         origin.addFilter("true");
